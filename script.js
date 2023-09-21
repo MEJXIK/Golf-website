f77f6ffd95c048d15b6ca254c5d2eaa73d717f2a
Max <90198615+MEJXIK@users.noreply.github.com>
2023-09-21 16:21:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function poka1(){
+    var video = document.querySelector(".video1");
+    video.classList.toggle("active");
+}
+function poka2(){
+    var video = document.querySelector(".video2");
+    video.classList.toggle("active");
+}
+function poka3(){
+    var video = document.querySelector(".video3");
+    video.classList.toggle("active");
+}
+function poka4(){
+    var video = document.querySelector(".video4");
+    video.classList.toggle("active");
+}
+function poka5(){
+    var video = document.querySelector(".video5");
+    video.classList.toggle("active");
+}
+function poka6(){
+    var video = document.querySelector(".video6");
+    video.classList.toggle("active");
+}
+function poka7(){
+    var video = document.querySelector(".video7");
+    video.classList.toggle("active");
+}
+function poka8(){
+    var video = document.querySelector(".video8");
+    video.classList.toggle("active");
+}
